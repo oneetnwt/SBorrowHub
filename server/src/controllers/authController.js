f25ce23d1ce6eb02da63707bfa703b7eef31d1cb
Jeff Ivan B. Mayor <jeffivanbiosanomayor@gmail.com>
2025-10-17 13:19:00 +0800

    Added bcryptjs package

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -6,7 +6,7 @@ import { appAssert } from "../errors/appAssert.js";
 export const signup = asyncHandler(async (req, res) => {
 	const body = createUserSchema.parse(req.body);
 
-	const user = await User.findOne({ studentId }).exec();
+	const user = await User.findOne({ studentId: body.studentId }).exec();
 	appAssert(!user, "User already exists", 400);
 
 	// const hashPassword = /
